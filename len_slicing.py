f33992d9f709df84f8e0f69b7a580cce0d74154f
Python_Basics <prajwalkatkar17@gmail.com>
2026-01-06 01:15:03 +0530

    functions

---
+++ len_slicing.py
@@ -0,0 +1,15 @@
+Name = "Prajwal"
+print(len(Name))#gives length of the string
+
+
+
+Namecount = Name[0:3]
+print(Namecount)
+
+ #this is called slicing in python 
+   
+
+   #some more example in negetive indexing 
+
+Hello = Name[-3:-1]
+print(Hello)
